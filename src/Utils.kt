3c9cec1a82f0eb24574e5fca898b357083567a53
Tobias Dötzer <tobias.doetzer@gmx.de>
2023-12-02 15:05:17 +0100

    Added solutions for day 2, readInput now trims lines

--- src/Utils.kt
+++ src/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("src/$name.txt").readLines().filterNot(String::isBlank)
 
 /**
  * Converts string to md5 hash.
